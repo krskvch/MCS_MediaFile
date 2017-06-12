a475253c7ecd97f39cc93272a58f0789ac616c07
krskvch <igor@krskv.ch>
2017-06-12 12:23:19 +0300

    добавил walk.os

--- media_bot.py
+++ media_bot.py
@@ -6,60 +6,74 @@ from MediaInfoDLL3 import *
 bot = telebot.TeleBot(config.API)
 MI = MediaInfo()
 outDict = {}
-# a = '/Users/igorkriskevich/Movies/MAY/4x3/05170104-VROOMIZ2_E04.mp4'
-# MI.Open(MediaInfo, a)
-# def writeChat(chat_id, message):
-#     bot.send_message(chat_id, message)
-
+filesDict = {}
 
 @bot.message_handler(content_types=["text"])
-def mediaParse(message):
-    messageOut = message.text
-    bot.send_message(message.chat.id, "Сканирую файл: " + messageOut)
-    MI.Open(messageOut)
+def file_listing(message):
+    messageStart = message.text
+    messageOut = ''
+    rootDir = '/Users/igorkriskevich/Movies/MAY/4x3'
+    files = []
+    for dirName, subdirList, fileList in os.walk(rootDir):
+        for fname in fileList:
+            if fname != '.DS_Store':
+                files.append(fname)
+    filesDict = dict(enumerate(files))
+    markup = types.ReplyKeyboardMarkup()
+    markup.row('START')
 
-    class file:
-        MI.Option_Static("Inform", "General;%FileName%.%FileExtension%")
-        outDict['File name'] = MI.Inform()
-        MI.Option_Static("Inform", "General;%FileSize/String2%")
-        outDict['File size'] = MI.Inform()
-        MI.Option_Static("Inform", "General;%Format%")
-        outDict['File format'] = MI.Inform()
-        MI.Option_Static("Inform", "General;%Duration/String4%")
-        outDict['Duration'] = MI.Inform()
+    if messageStart == 'START':
+        for key in filesDict:
+            messageOut = messageOut + str(key) + ': ' + filesDict[key] + '\n'
+        bot.send_message(message.chat.id, "Выберите файл: " + '\n' + messageOut)
 
-    class video:
-        MI.Option_Static("Inform", "Video;%CodecID%")
-        outDict['Codec'] = MI.Inform()
-        MI.Option_Static("Inform", "Video;%BitRate_Mode/String%, %BitRate/String%")
-        outDict['Bit rate'] = MI.Inform()
-        MI.Option_Static("Inform", "Video;%FrameRate/String%")
-        outDict['Frame rate'] = MI.Inform()
-        MI.Option_Static("Inform", "Video;%ScanOrder/String%")
-        outDict['Field order'] = MI.Inform()
-        MI.Option_Static("Inform", "Video;%Width%x%Height%")
-        outDict['Resolution'] = MI.Inform()
-        MI.Option_Static("Inform", "Video;%PixelAspectRatio%")
-        outDict['Aspect'] = MI.Inform()
+    else:
+        messageOut = message.text
+        bot.send_message(message.chat.id, "Сканирую файл: " + filesDict[int(messageOut)])
+        MI.Open(rootDir + '/' + filesDict[int(messageOut)])
 
-    class audio:
-        MI.Option_Static("Inform", "Audio;%Format%")
-        outDict['Audio codec'] = MI.Inform()
-        MI.Option_Static("Inform", "Audio;%BitRate/String%")
-        outDict['Audio bit rate'] = MI.Inform()
-        MI.Option_Static("Inform", "Audio;%SamplingRate/String%")
-        outDict['Sample rate'] = MI.Inform()
-        MI.Option_Static("Inform", "Audio;%BitDepth_Detected%")
-        outDict['Sample size'] = MI.Inform()
+        class file:
+            MI.Option_Static("Inform", "General;%FileName%.%FileExtension%")
+            outDict['File name'] = MI.Inform()
+            MI.Option_Static("Inform", "General;%FileSize/String2%")
+            outDict['File size'] = MI.Inform()
+            MI.Option_Static("Inform", "General;%Format%")
+            outDict['File format'] = MI.Inform()
+            MI.Option_Static("Inform", "General;%Duration/String4%")
+            outDict['Duration'] = MI.Inform()
+
+        class video:
+            MI.Option_Static("Inform", "Video;%CodecID%")
+            outDict['Codec'] = MI.Inform()
+            MI.Option_Static("Inform", "Video;%BitRate_Mode/String%, %BitRate/String%")
+            outDict['Bit rate'] = MI.Inform()
+            MI.Option_Static("Inform", "Video;%FrameRate/String%")
+            outDict['Frame rate'] = MI.Inform()
+            MI.Option_Static("Inform", "Video;%ScanOrder/String%")
+            outDict['Field order'] = MI.Inform()
+            MI.Option_Static("Inform", "Video;%Width%x%Height%")
+            outDict['Resolution'] = MI.Inform()
+            MI.Option_Static("Inform", "Video;%PixelAspectRatio%")
+            outDict['Aspect'] = MI.Inform()
+
+        class audio:
+            MI.Option_Static("Inform", "Audio;%Format%")
+            outDict['Audio codec'] = MI.Inform()
+            MI.Option_Static("Inform", "Audio;%BitRate/String%")
+            outDict['Audio bit rate'] = MI.Inform()
+            MI.Option_Static("Inform", "Audio;%SamplingRate/String%")
+            outDict['Sample rate'] = MI.Inform()
+            MI.Option_Static("Inform", "Audio;%BitDepth_Detected%")
+            outDict['Sample size'] = MI.Inform()
 
 
-        MI.Option_Static("Inform", "Audio;%Channel(s)/String%, %ChannelLayout%")
-        outDict['Channels layout'] = MI.Inform()
+            MI.Option_Static("Inform", "Audio;%Channel(s)/String%, %ChannelLayout%")
+            outDict['Channels layout'] = MI.Inform()
 
 
-    for key in outDict:
-        messageOut = key + '-' + outDict[key]
-    bot.send_message(message.chat.id, messageOut)
+        for key in outDict:
+            messageOut = messageOut + key + '-' + outDict[key] + '\n'
+        bot.send_message(message.chat.id, messageOut)
 
 
 
